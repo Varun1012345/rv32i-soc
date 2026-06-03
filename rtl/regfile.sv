2215f8f22baecf49962308477fd53353dfcc4548
Varun Kumar C V <cvvarunkumar@gmail.com>
2026-06-03 11:44:58 +0200

    Setup ModelSim simulation flow

---
+++ rtl/regfile.sv
@@ -0,0 +1,26 @@
+module regfile (
+    input  logic        clk,
+    input  logic        we,
+
+    input  logic [4:0]  rs1,
+    input  logic [4:0]  rs2,
+    input  logic [4:0]  rd,
+
+    input  logic [31:0] wd,
+
+    output logic [31:0] rd1,
+    output logic [31:0] rd2
+);
+
+logic [31:0] regs [31:0];
+
+assign rd1 = (rs1 == 5'd0) ? 32'd0 : regs[rs1];
+assign rd2 = (rs2 == 5'd0) ? 32'd0 : regs[rs2];
+
+always_ff @(posedge clk) begin
+    if (we && (rd != 5'd0)) begin
+        regs[rd] <= wd;
+    end
+end
+
+endmodule
